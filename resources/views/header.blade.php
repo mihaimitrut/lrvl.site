3ab6b7b9eee8811e3baf3dfa9a28bf4e06463e11
Mihai Florin MITRUT <mmitrut@bbu.bitdefender.biz>
2016-09-06 19:16:53 +0300

    added analytics

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -42,4 +42,16 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     </script>
     <!---//End-rate---->
     <link href="{{ asset('css/form.css') }}" rel="stylesheet" type="text/css" >
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-83749069-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+    
 </head>
