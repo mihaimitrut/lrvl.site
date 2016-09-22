ca6f8732ac4eeab3920f91195c0541936879a26c
Mihai Florin MITRUT <mmitrut@bbu.bitdefender.biz>
2016-09-22 13:22:22 +0300

    removed checkout.html link

--- resources/views/categories_menu2.blade.php
+++ resources/views/categories_menu2.blade.php
@@ -6,12 +6,7 @@
     {{--<li><a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i></a></li>--}}
 </ul>
 <div class="cart box_1">
-    <a href="checkout.html">
-        {{--<h3> <div class="total">--}}
-                {{--<span class="simpleCart_total"></span>--}}
-            {{--</div>--}}
-            {{--<img src="images/cart.png" alt=""/></h3>--}}
-    </a>
+
     {{--<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>--}}
 
 </div>
